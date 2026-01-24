a975071bd258326e829284171894e21eb058537f
nt-ossas <ossasiocil@gmail.com>
2026-01-24 23:31:09 +0100

    Fix login

--- src/modules/Login.jsx
+++ src/modules/Login.jsx
@@ -2,8 +2,8 @@ import { useState } from "react";
 import "./login.css";
 
 export function Login({ onLogin, apiUrl }) {
-  const [email, setEmail] = useState("mario@test.com");
-  const [password, setPassword] = useState("test123");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
 
